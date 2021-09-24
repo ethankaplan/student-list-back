e83df860ab7eb71c2e84a0345d41b590a704db4e
ethankaplan <phaladone@gmail.com>
2021-09-23 17:57:04 -0700

    Probable Cors

--- server.js
+++ server.js
@@ -26,16 +26,12 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 app.use(cors());
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://student-list-fe.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
