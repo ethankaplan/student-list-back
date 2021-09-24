e7e13544d2e9d5c0dcf62670bd3fe4a290e8292a
ethankaplan <phaladone@gmail.com>
2021-09-23 18:43:16 -0700

    added online database for production

--- database/db.js
+++ database/db.js
@@ -1,3 +1,10 @@
+if(process.env.ON_HEROKU!==1){
 module.exports = {
     db: 'mongodb://localhost:27017/reactdb'
+    
   };
+}else{
+  module.exports = {
+    db: `mongodb+srv://1:${process.env.BE_PW}@cluster0.1m3su.mongodb.net/students?retryWrites=true&w=majority`
+  };
+}
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.set('useFindAndModify', false);
 mongoose.connect(dbConfig.db, {
   useNewUrlParser: true
 }).then(() => {
-  console.log('Database sucessfully connected!')
+  console.log('Database successfully connected!')
 },
   error => {
     console.log('Could not connect to database : ' + error)
@@ -28,24 +28,6 @@ app.use(express.urlencoded({
 }));
 
 app.use(cors());
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 app.use('/students', studentRoute)
 app.use('/user',userRoute)
 
