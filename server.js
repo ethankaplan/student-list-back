35ed0d959f909b62d89bac4dd75672ec7e1e4b42
ethankaplan <phaladone@gmail.com>
2021-09-23 20:11:52 -0700

    more cors flailing

--- server.js
+++ server.js
@@ -22,29 +22,19 @@ mongoose.connect(dbConfig.db, {
 )
 
 const app = express();
-
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
+app.use(cors({
+  origin:'*',
+  credentials:true,
+  
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
-app.use(allowCrossDomain);
-app.use(cors());
 app.use('/students', studentRoute)
 app.use('/user',userRoute)
+
 
 
 // PORT
